a4c8b3da78f8b750890b04b48b5f991abe4d0108
sparethme <spare@ualberta.ca>
2020-04-21 13:05:41 -0400

    Updates

---
+++ precache-manifest.a866421c98eef685fff434ed204e9579.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a87c0f9d38c0e93e97ac3674fb1c3c5",
+    "url": "/sparethme/react-my-app-test.git/index.html"
+  },
+  {
+    "revision": "9a569f51d7d4f8ab9a6f",
+    "url": "/sparethme/react-my-app-test.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c894e8a8e01d6a599ce8",
+    "url": "/sparethme/react-my-app-test.git/static/js/2.07a05dc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sparethme/react-my-app-test.git/static/js/2.07a05dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a569f51d7d4f8ab9a6f",
+    "url": "/sparethme/react-my-app-test.git/static/js/main.5b1abdd4.chunk.js"
+  },
+  {
+    "revision": "0d40ad10fcddd542e0d6",
+    "url": "/sparethme/react-my-app-test.git/static/js/runtime-main.a054caa1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sparethme/react-my-app-test.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
